730e61fe7c5accf49cdcaf4b3e8737b2acd2f056
Vinicius Nunes <vnunes@technomar.com.br>
2021-12-28 10:06:54 -0300

    Classe com '-'

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,29 @@
-import { BannerAd } from '../components/BannerAd';
-import styles from '../styles/home.module.scss';
+import { BannerAd } from "../components/BannerAd";
+import styles from "../styles/home.module.scss";
 
 export default function Home() {
   return (
     <>
-      <div className={styles.containerAlert}>
+      <div className={`${styles["container-Alert"]} ${styles.test}`}>
         <p>
-          <a href="">Compre online</a> com assistência de especialistas, entrega 
+          <a href="">Compre online</a> com assistência de especialistas, entrega
           grátis e muito mais.
         </p>
       </div>
 
-      <BannerAd 
-        title="iPhone 12" 
+      <BannerAd
+        title="iPhone 12"
         subtitle="Roxinho básico."
-        textAlert="" 
+        textAlert=""
         image="/hero_iphone_12_purple__eybnx2ju5mky_large.jpg"
         textFirstButton="Saiba mais"
         textSecondButton="Comprar"
         dark={false}
       />
 
-      <BannerAd 
-        title="iMac" 
-        subtitle="Diga olá." 
+      <BannerAd
+        title="iMac"
+        subtitle="Diga olá."
         textAlert="Confira em breve a disponibilidade"
         image="/hero_imac_avail__zbofyl96mzmm_large.jpg"
         textFirstButton="Saiba mais"
@@ -31,9 +31,9 @@ export default function Home() {
         dark={false}
       />
 
-      <BannerAd 
-        title="iPhone 12 Pro" 
-        subtitle="Um salto radical." 
+      <BannerAd
+        title="iPhone 12 Pro"
+        subtitle="Um salto radical."
         textAlert=""
         image="/iphone_12_pro_us__e5oyysg4k0ya_large.jpg"
         textFirstButton="Saiba mais"
